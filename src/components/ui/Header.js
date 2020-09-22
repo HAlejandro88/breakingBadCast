a312cee28a72e48c8247e2d8e70154a906f99d06
Alejandro <alexsotog82@gmail.com>
2020-09-21 21:30:17 -0500

    app cmpleta de breaking bad con filtro 💡💻

---
+++ src/components/ui/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../../img/logo.png';
+
+const header = () => {
+    return (
+        <header className="center">
+            <img src={ logo } alt='logoP' />
+        </header>
+    )
+}
+
+export default header
